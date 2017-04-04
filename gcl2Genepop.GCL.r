7ed563a882af16c606e7fb3229b5d8da78002996
Shedd <kyle.shedd@dfg.alaska.local>
2017-04-04 13:08:48 -0800

    Update gcl2Genepop.GCL to output uSat scores
    Added a logical switch "usat" to print out the scores rather than counts when set to TRUE. Default setting is FALSE.

--- gcl2Genepop.GCL.r
+++ gcl2Genepop.GCL.r
@@ -1,4 +1,4 @@
-gcl2Genepop.GCL=function(sillyvec,loci,path,VialNums=TRUE){
+gcl2Genepop.GCL=function(sillyvec,loci,path,VialNums=TRUE,usat=FALSE){
 
   if(sum(is.na(match(loci,LocusControl$locusnames)))){stop(paste("'",loci[is.na(match(loci,LocusControl$locusnames))],"' from argument 'loci' not found in 'LocusControl' object!!!",sep=""))}
 
@@ -22,8 +22,15 @@ gcl2Genepop.GCL=function(sillyvec,loci,path,VialNums=TRUE){
     IDs=dimnames(my.gcl$scores)[[1]]
     names(vials)=IDs    
     scores=my.gcl$scores[,loci,]
-    scores <- gsub(pattern = "0", replacement = NA, x = scores)
-    counts=sapply(IDs,function(ID){paste(sapply(loci,function(locus){paste(sapply(1:ploidy[locus],function(ploid){ifelse(is.na(scores[ID,locus,ploid]),paste(rep(0,maxchar[locus]),collapse=""),paste(c(rep(0,maxchar[locus]-nchar(match(scores[ID,locus,ploid],alleles[[locus]]))),match(scores[ID,locus,ploid],alleles[[locus]])),collapse=""))}),collapse="")}),collapse=" ")})
+    scores[scores == "0"] <- NA
+    if(usat) {
+      scores[is.na(scores)] <- paste(rep(0, max(maxchar)), collapse = '')
+      scores[nchar(scores) < max(maxchar)] <- paste0(0, scores[nchar(scores) < max(maxchar)])
+      scores <- matrix(data = paste0(scores[, , 1], scores[, , 2]), nrow = nrow(scores), ncol = ncol(scores), dimnames = list(rownames(scores), colnames(scores)))
+      counts <- apply(scores, 1, function(ind) {paste(ind, collapse = ' ')})
+    } else {
+      counts=sapply(IDs,function(ID){paste(sapply(loci,function(locus){paste(sapply(1:ploidy[locus],function(ploid){ifelse(is.na(scores[ID,locus,ploid]),paste(rep(0,maxchar[locus]),collapse=""),paste(c(rep(0,maxchar[locus]-nchar(match(scores[ID,locus,ploid],alleles[[locus]]))),match(scores[ID,locus,ploid],alleles[[locus]])),collapse=""))}),collapse="")}),collapse=" ")})
+    }
     counts=as.character(sapply(IDs,function(ID){paste(paste(vials[ID]," , ",sep=""),counts[ID],collapse=" ")}))
     file=rbind(file,cbind(c("Pop",counts)))
   }
