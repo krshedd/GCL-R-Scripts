53a063e982296999bae1458dff7ab0ada0e82139
krshedd <kyle.shedd@alaska.gov>
2018-05-08 15:36:05 -0800

    Update create_rubias_baseline.R - automatically saves a .csv of baseline

--- create_rubias_baseline.R
+++ create_rubias_baseline.R
@@ -1,4 +1,4 @@
-create_rubias_baseline <- function(sillyvec, loci, group_names, groupvec) {
+create_rubias_baseline <- function(sillyvec, loci, group_names, groupvec, path, baseline_name) {
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # This function creates the baseline dataframe needed for `rubias`.
   # It reformats the "scores" from each individual in the baseline into a two column format used by `rubias`.
@@ -8,14 +8,21 @@ create_rubias_baseline <- function(sillyvec, loci, group_names, groupvec) {
   #   loci - character vector of the loci you wish to include
   #   group_names - character vector of group names
   #   groupvec - numeric vector indicating the group affiliation of each pop in sillyvec
+  #   path - character vector of where to save each mixture as a .csv
+  #   baseline_name - character vector of what to name the baseline.csv
   #
   # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #   Returns a dataframe in `rubias` baseline format
+  #   Saves the baseline as a .csv file for posterity
+  #     NOTE: to read in these .csv's use `read_csv(file = file, col_types = cols(.default = "c"))` 
+  #     to make sure all columns are character vectors (if homozygous for T, it will become a logical vector).
   #
   # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # load("V:/Analysis/4_Westward/Sockeye/Chignik Inseason 2012-2017/Baseline 2012/Chignik2012SockeyeBaseline.RData")
-  # chignik_7pops_22loci.rubias_base <- create_rubias_baseline(sillyvec = Chignik7Populations, loci = loci24, group_names = Groups, groupvec = Groupvec7)
+  # chignik_7pops_22loci.rubias_base <- create_rubias_baseline(sillyvec = Chignik7Populations, loci = loci24, group_names = Groups, groupvec = Groupvec7, path = "rubias/baseline", baseline_name = "chignik_7pops_22loci")
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  while(!require(tidyverse)){install.packages("tidyverse")}
+  
   silly_base.lst <- lapply(sillyvec, function(silly) {
     my.gcl <- get(paste0(silly, ".gcl"))
     scores.mat <- t(apply(my.gcl$scores[, loci, ], 1, function(ind) {c(t(ind))} ))
@@ -27,5 +34,7 @@ create_rubias_baseline <- function(sillyvec, loci, group_names, groupvec) {
     scores.df$indiv <- as.character(my.gcl$attributes$SillySource)
     silly_base.df <- scores.df[, c("sample_type", "repunit", "collection", "indiv", gsub(pattern = "-", replacement = ".", x = colnames(scores.mat)))] } #silly
   )
-  return(do.call("rbind", silly_base.lst))
+  baseline <- do.call("rbind", silly_base.lst)
+  write_csv(x = baseline, path = paste0(path, "/", baseline_name, "_base.csv"))
+  return(baseline)
 }
