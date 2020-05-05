99845c3d82efb98be51b2823808778c31115aa08
Barclay <andy.barclay@dfg.alaska.local>
2020-05-05 10:25:34 -0800

    First commit for CreateLocusControl.GCL. This was tested using markersuite and locusnames and it works.

--- CreateLocusControl.GCL.R
+++ CreateLocusControl.GCL.R
@@ -1,129 +1,151 @@
-CreateLocusControl.GCL <- function(markersuite = NULL, locusnames = NULL, username, password) {
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #
-  # This function connects to LOKI and creates a "LocusControl" object  
-  # User needs to supply either "markersuite" OR "locusnames" (character vector of loci)
-  # 
-  # "markersuite" is the pre-defined name in LOKI for the set of markers for which you want genotypes (e.g. markersuite="KenaiChinook2010_40SNPs"). 
-  #   This set must be pre-defined in LOKI (see Eric Lardizabal). 
-  #
-  # "locusnames" is a character vector of marker names for which you want genotypes.
-  #   i.e. c("One_CO1", "One_GPDH", "One_MHC2_190)
-  #
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #### Example/Intended ####
-  #
-  # markersuite <- "Sockeye2011_96SNPs"
-  #  
-  # username <- "jjasper"; password <- "********"
-  #
-  # Written by JJ 10/05/2015
-  # updated to new ojdbc6.jar path on V:/Analysis by Kyle Shedd on 05/05/2016
-  # Updated to add 'locusnames' argument by Kyle Shedd on 01/10/2018
-  # Updated by Andy Barclay 4/15/19; updated driver from ojdbc6.jar to ojdbc8.jar and changed the LOKI connection URL
-  # to connect to the new Oracle cloud database
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    if(exists("LocusControl",where=1)){
-        stop("LocusControl already exists")
-    }
-
-    while(!require(RJDBC)){install.packages("RJDBC")}
-
-    if(!file.exists(path.expand("~/R"))){
+CreateLocusControl.GCL <- function(markersuite = NULL, locusnames = NULL, username, password){
   
-     dir<-path.expand("~/R")
-                                                   
-     dir.create(dir)
-      
-     bool <- file.copy(from="V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar",to=path.expand("~/R/ojdbc8.jar"))
-      
-     } else {
-      
-     if(!file.exists(path.expand("~/R/ojdbc8.jar"))){
-         
-      bool <- file.copy(from="V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar",to=path.expand("~/R/ojdbc8.jar"))
-      
-      }
-      
-     }
- 
-    options(java.parameters = "-Xmx10g")
-
-    if(file.exists("C:/Program Files/R/RequiredLibraries/ojdbc8.jar")) {
-
-      drv <- JDBC("oracle.jdbc.OracleDriver",classPath="C:/Program Files/R/RequiredLibraries/ojdbc8.jar"," ")#https://blogs.oracle.com/R/entry/r_to_oracle_database_connectivity    C:/app/awbarclay/product/11.1.0/db_1/jdbc/lib
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  This function connects to LOKI and creates a "LocusControl" object  
+  #
+  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   markersuite - "markersuite" is the pre-defined name in LOKI for the set of markers for which you want genotypes (e.g. markersuite="KenaiChinook2010_40SNPs"). 
+  #                  This set must be pre-defined in LOKI (see Eric Lardizabal). 
+  #
+  #   locusnames - a character vector of locus names spelled exactly the way they are in LOKI.
+  #                Use Locus Report Brief in Ocean AK to get the correct locus names.
+  #
+  #   username - your state user name
+  #
+  #   password - your password used to access LOKI - see Eric Lardizabal if you don't have a passord for LOKI
+  #
+  # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   This function assigns a tibble with the following columns: Markersuite <character>: markersuite name, 
+  #                                                              locusnames <character>: the locus name used by the GCL, 
+  #                                                              Publishedlocusnames<character>: the locus name used in publications,
+  #                                                              nalleles <int>: the number of alleles for each locus,
+  #                                                              ploidy <int>: the ploidy of the locus (2 = diploid, 1 = haploid),
+  #                                                              alleles <named list>: named list of tibbles containing the alleles for each locus
+  #   The tibble will be named "LocusControl"
+  #
+  # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #
+  #   password = "************"
+  # 
+  #   CreateLocusControl.GCL(markersuite = "UCI_Chinook_GTSeq_557SNPs", locusnames = NULL, username = "awbarclay", password = password)
+  #
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+  
+  if(exists("LocusControl", where = 1)){
     
-    } else {
+    stop("LocusControl already exists")
+    
+  }
+  
+  if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(RJDBC, tidyverse) #Install packages, if not in library and then load them.
+  
+  # This copies the "odbc8.jar" file to the R folder on your computer if it doesn't exist there. This file contains the java odbc drivers needed for RJDBC
+  
+  if(!file.exists(path.expand("~/R"))){ 
+    
+    dir <- path.expand("~/R")
+    
+    dir.create(dir)
       
-      drv <- JDBC("oracle.jdbc.OracleDriver",classPath=path.expand("~/R/ojdbc8.jar")," ")
+    bool <- file.copy(from="V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar", to = path.expand("~/R/ojdbc8.jar"))
+      
+    } else{
+      
+      if(!file.exists(path.expand("~/R/ojdbc8.jar"))){
+        
+        bool <- file.copy(from = "V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar", to = path.expand("~/R/ojdbc8.jar"))
+        
+       }
       
     }
+  
+  options(java.parameters = "-Xmx10g")
+  
+  if(file.exists("C:/Program Files/R/RequiredLibraries/ojdbc8.jar")) {
     
-    url <-LOKI_URL.GCL()
+    drv <- RJDBC::JDBC("oracle.jdbc.OracleDriver", classPath = "C:/Program Files/R/RequiredLibraries/ojdbc8.jar", " ")#https://blogs.oracle.com/R/entry/r_to_oracle_database_connectivity    C:/app/awbarclay/product/11.1.0/db_1/jdbc/lib
     
-    con <- dbConnect(drv,url=url,user=username,password=password)
-    
-    # Query by 'markersuite', else query by 'locusnames'
-
-    if(is.null(markersuite) & is.null(locusnames)) {stop("Need to provide either 'locusnames' or 'markersuite'")}
-    
-    if(is.null(locusnames) & !is.null(markersuite)) {
-      
-      lociqry <- paste("SELECT * FROM AKFINADM.V_LOCUSQRY WHERE SUITE_NAME = '",markersuite,"'",sep="")  # Query locus information of markers in markersuite.
-      
-      locidata <- dbGetQuery(con,lociqry)
-      
-      locusnames <- sort(locidata$LOCUS_NAME)
-
     } else {
-
+      
+      drv <- RJDBC::JDBC("oracle.jdbc.OracleDriver", classPath = path.expand("~/R/ojdbc8.jar"), " ")
+      
+      } 
+    
+  url <- LOKI_URL.GCL() #This is a function that gets the correct URL to access the database on the oracle cloud
+    
+  con <- RJDBC::dbConnect(drv,url=url,user=username,password=password)
+  
+  # Query by 'markersuite', else query by 'locusnames'
+  
+  if(is.null(markersuite) & is.null(locusnames)) {stop("Need to provide either 'locusnames' or 'markersuite'")}
+  
+  if(is.null(locusnames) & !is.null(markersuite)) {
+      
+    lociqry <- paste("SELECT * FROM AKFINADM.V_LOCUSQRY WHERE SUITE_NAME = '", markersuite, "'", sep = "")  # Query locus information of markers in markersuite.
+      
+    locidata <- RJDBC::dbGetQuery(con, lociqry)
+      
+    locusnames <- sort(locidata$LOCUS_NAME)
+    
+    } else {
+      
       markersuite <- "User defined from locusnames"
       
-      lociqry <- paste("SELECT * FROM AKFINADM.V_LOCUSQRY WHERE LOCUS_NAME IN (",paste0("'",locusnames,"'",collapse=","),")",sep="")  # Query locus information of markers in locusnames.
+      lociqry <- paste("SELECT * FROM AKFINADM.V_LOCUSQRY WHERE LOCUS_NAME IN (",paste0("'", locusnames, "'", collapse = ","), ")", sep = "")  # Query locus information of markers in locusnames.
       
-      locidata <- dbGetQuery(con,lociqry)
+      locidata <- RJDBC::dbGetQuery(con, lociqry)
       
-    }
-    
+      }
   
-    # Warn user if some 'locusnames' not found
-    if(!all(locusnames %in% locidata$LOCUS_NAME)) {
+  # Warn user if some 'locusnames' not found
+  if(!all(locusnames %in% locidata$LOCUS_NAME)) {
     
-      miss_loci <- locusnames[!locusnames %in% locidata$LOCUS_NAME]
+    miss_loci <- locusnames[!locusnames %in% locidata$LOCUS_NAME]
       
-      nmiss_loci <- length(miss_loci)
+    nmiss_loci <- length(miss_loci)
       
-      message(paste(nmiss_loci, "out of", length(locusnames), "locusnames not found in LOKI!!!"))
-        
-      sapply(miss_loci, function(locus) {
-        message(locus)
-      } )
+    message(paste(nmiss_loci, "out of", length(locusnames), "locusnames not found in LOKI!!!"))
+    
+    sapply(miss_loci, function(locus) {
       
-      locusnames <- locusnames[locusnames %in% locidata$LOCUS_NAME]
+      message(locus)
       
+      })
+    
+    locusnames <- locusnames[locusnames %in% locidata$LOCUS_NAME]
+    
     }
+  
+  Publishedlocusnames <- purrr::set_names(locidata$PUBLISHED_NAME, locidata$LOCUS_NAME)[locusnames]
     
-    Publishedlocusnames <- setNames(locidata$PUBLISHED_NAME,locidata$LOCUS_NAME)[locusnames]
+  nloci <- length(locusnames)
     
-    nloci <- length(locusnames)
+  ploidy <- purrr::set_names(locidata$PLOIDY, locidata$LOCUS_NAME)[locusnames]
     
-    ploidy <- setNames(locidata$PLOIDY,locidata$LOCUS_NAME)[locusnames]
-    
-    alleleqry <- paste("SELECT * FROM AKFINADM.V_ALLELEQRY WHERE LOCUS IN (",paste0("'",locusnames,"'",collapse=","),")",sep="")#Get possible alleles from allele lookup table.
+  alleleqry <- paste("SELECT * FROM AKFINADM.V_ALLELEQRY WHERE LOCUS IN (", paste0("'", locusnames, "'", collapse = ","), ")", sep = "")#Get possible alleles from allele lookup table.
 
-    alleles0 <- dbGetQuery(con,alleleqry)
+  alleles0 <- RJDBC::dbGetQuery(con, alleleqry)
 
-    discon <- dbDisconnect(con)
-
-    alleles <- tapply(X=alleles0$VALUE,INDEX=list(alleles0$LOCUS),FUN=sort,simplify=FALSE)[locusnames]
+  discon <- RJDBC::dbDisconnect(con) #Disconnect from database
+  
+  alleles <- lapply(locusnames, function(locus){
+  
+    my.loc = dplyr::filter(alleles0, LOCUS==locus) %>% 
+      dplyr::select(-VALUE_CTC)
+   
+    nalleles = dim(my.loc)[1] 
     
-    nalleles <- sapply(alleles,length)
+    tibble::tibble(allele = seq(nalleles), call = my.loc$VALUE %>% sort())
+    
+  }) %>% purrr::set_names(locusnames)
+    
+  nalleles <- sapply(alleles, dim)[1,] 
       
-    assign("LocusControl",list(MarkerSuite=markersuite,locusnames=locusnames,Publishedlocusnames=Publishedlocusnames,alleles=alleles,nalleles=nalleles,ploidy=ploidy),pos=1) #Assign elements to LocusControl list.	
+  assign("LocusControl", tibble::tibble(MarkerSuite = markersuite, locusnames = locusnames, Publishedlocusnames = Publishedlocusnames, nalleles = nalleles, ploidy = ploidy, alleles = alleles), pos = 1, envir = .GlobalEnv) #Assign elements to LocusControl list.	
 
-    ans <- paste0("LocusControl created for markersuite='",markersuite,"'")
+  ans <- paste0("LocusControl created for markersuite ='", markersuite, "'")
     
-    return(ans)
+  return(ans)
 
 }  
    
