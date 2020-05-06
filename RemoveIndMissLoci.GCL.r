0706ab80f0fc12a8d28359588435c20ac6782803
Barclay <andy.barclay@dfg.alaska.local>
2020-05-06 15:25:47 -0800

    Added loci argument, documentation, and package names:: to functions in RemoveIndMissLOci.GCL

--- RemoveIndMissLoci.GCL.r
+++ RemoveIndMissLoci.GCL.r
@@ -1,4 +1,4 @@
-RemoveIndMissLoci.GCL <- function(sillyvec, proportion = 0.8){
+RemoveIndMissLoci.GCL <- function(sillyvec, loci = NULL, proportion = 0.8){
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #   This function removes individuals from "*.gcl" objects that have fewer non-missing loci than that specified by "proportion".
@@ -8,6 +8,9 @@ RemoveIndMissLoci.GCL <- function(sillyvec, proportion = 0.8){
   #   sillyvec - a vector of silly codes without the ".gcl" extention (e.g. sillyvec <- c("KQUART06","KQUART08","KQUART10")). 
   #
   #   proportion - the cut-off proportion of the number of non-missing loci.
+  #
+  #   loci - optional vector of locus names to be considered when removing individuals for missing data; default is LocusControl$locusnames
+  #          This argument is useful if you have loci in your markersuite that do not perform well in the lab and they will be dropped from the final dataset. 
   # 
   # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #    Returns a tibble of indiduals removed by silly.
@@ -18,30 +21,53 @@ RemoveIndMissLoci.GCL <- function(sillyvec, proportion = 0.8){
   # 
   #   missloci_ind <- RemoveIndMissLoci.GCL(sillyvec = sillyvec157, proportion = 0.8)
   #
+  # Note~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #
+  # This function only removes individuals for failed scores (zero scores), not unanalyzed loci (NAs).
+  # Therefore, individuals with a lot of NAs will not be removed. Use RemoveNAindv.GCL to remove individuals with NAs for all loci.
+  #
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
+  if(!exists("LocusControl")){
+    
+    stop("'LocusControl' not yet built.")
+    
+  }
+  
   if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse) #Install packages, if not in library and then load them.
+  
+  if(is.null(loci)){
+    
+    loci <- LocusControl$locusnames #Use all loci if loci argument set to NULL.
+    
+  }
+  
+  if(!length(loci)==sum(loci%in%LocusControl$locusnames)){
+    
+   stop("The following loci are not in LocusControl-", paste0(loci[- na.omit(match(LocusControl$locusnames, loci))], collapse = " ,")) #Making sure loci are spelled correctly.
+    
+  }
   
   output <- lapply(sillyvec, function(silly){
     
     my.gcl <- get(paste0(silly, ".gcl"), pos = 1)
     
     tmp <- my.gcl %>% 
-      select(LocusControl$locusnames) 
+      dplyr::select(loci) 
     
     IDsToRemove <- my.gcl %>% 
-      mutate(nloci = rowSums(!is.na(tmp)), nmissing = rowSums(tmp == "0")) %>% 
-      mutate(prop_loci = 1-(nmissing/nloci)) %>% 
-      select(prop_loci, everything()) %>% 
-      filter(prop_loci <= proportion) %>% 
-      pull(FK_FISH_ID)
+      dplyr::mutate(nloci = rowSums(!is.na(tmp)), nmissing = rowSums(tmp == "0")) %>% 
+      dplyr::mutate(prop_loci = 1-(nmissing/nloci)) %>% 
+      dplyr::select(prop_loci, everything()) %>% 
+      dplyr::filter(prop_loci <= proportion) %>% 
+      dplyr::pull(FK_FISH_ID)
     
-    assign(x = paste0(silly, ".gcl"), value = my.gcl %>% filter(!FK_FISH_ID%in%IDsToRemove), pos = 1, envir = .GlobalEnv )
+    assign(x = paste0(silly, ".gcl"), value = my.gcl %>% dplyr::filter(!FK_FISH_ID%in%IDsToRemove), pos = 1, envir = .GlobalEnv )
     
-    tibble(SILLY_CODE = silly, IDs_Removed = IDsToRemove)
+    tibble::tibble(SILLY_CODE = silly, IDs_Removed = IDsToRemove)
     
   }) %>% 
-    bind_rows()
+    dplyr::bind_rows()
   
  if(dim(output)[1]==0){
    
