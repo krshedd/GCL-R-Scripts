cd7b1f3184698ccc523d31edce08b8a77626e4c9
Barclay <andy.barclay@dfg.alaska.local>
2020-05-13 09:42:22 -0800

    Added quantile functionality to CheckDupWithinSilly.GCL. Also cleaned up code and added packages to non base functions. This version works line by line, but needs testing as a function.

--- CheckDupWithinSilly.GCL.r
+++ CheckDupWithinSilly.GCL.r
@@ -1,4 +1,4 @@
-CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproportion = 0.95, ncores = 4){
+CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproportion = 0.95, ncores = 8){
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #   This function checks for duplicate individuals within each silly in "sillyvec".
@@ -14,16 +14,18 @@ CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproporti
   #Setting "quantile" equal to NULL will skip the calculation of the duplication distribution and will run much faster--REQUIRES the package "reshape" ##### Need to use something other than reshape
   # 
   # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #    Returns a tibble of duplicate pairs of individuals by silly.
+  #    When quantile is set to NULL, returns a tibble of duplicate pairs of individuals by silly.
+  #    When quantile is a number, a list containing a tibble of duplicate pairs of individuals by silly and tibble the porportion of duplication for each pair of individuals.
   #
   # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #   load("V:/Analysis/2_Central/Chinook/Cook Inlet/2019/2019_UCI_Chinook_baseline_hap_data/2019_UCI_Chinook_baseline_hap_data.RData")
   # 
-  #   dupcheck <- CheckDupWithinSilly.GCL(sillyvec = sillyvec157, loci = loci557, quantile = 0.99, minproportion = 0.95)
+  #   dupcheck <- CheckDupWithinSilly.GCL(sillyvec = sillyvec157, loci = loci557, quantile = 0.99, minproportion = 0.95, ncores = 4)
   #
   # Note~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #
+  #   Sillys without duplicates will be included in the output report but will have NAs for IDs1, IDs2, Missing1, Missing2, and proportion
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  start.time <- Sys.time() 
   
   if(sum(is.na(match(loci, LocusControl$locusnames)))){
     
@@ -32,6 +34,11 @@ CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproporti
   }
   
   if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse, reshape, doParallel, foreach) #Install packages, if not in library and then load them.
+  
+  
+  if(ncores > detectCores()) {
+    stop("'ncores' is greater than the number of cores available on machine\nUse 'detectCores()' to determine the number of cores on your machine")
+  }
 
   nsilly <- length(sillyvec)
 
@@ -40,19 +47,22 @@ CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproporti
   scores_cols <- c(loci, paste0(loci, ".1")) %>% 
     sort()
   
-  if (.Platform$OS.type == "windows") flush.console()
+  my.gcl <- lapply(sillyvec, function(silly){
+    
+    get(paste(silly, ".gcl", sep=""), pos = 1)
+    
+    }) %>% purrr::set_names(sillyvec)
   
-  my.gcl <- lapply(sillyvec,function(silly){get(paste(silly,".gcl",sep=""),pos=1)})
-  names(my.gcl)=sillyvec
-  
+  #Start if NULL quantile
   if(is.null(quantile)){
     
-    cl <- parallel::makePSOCKcluster(ncores)
+    cl <- doParallel::makePSOCKcluster(ncores)
     
-    registerDoParallel(cl, cores=ncores)  
+    doParallel::registerDoParallel(cl, cores=ncores)  
     
     # multicore loop
-    dupcheck <-  foreach(silly = sillyvec) %dopar% {
+    
+    dupcheck <- foreach::foreach(silly = sillyvec) %dopar% {
       
       new.gcl <- my.gcl[[silly]]
         
@@ -82,8 +92,9 @@ CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproporti
             
             })
           
-          }) %>% t() %>% 
-          data.frame ()
+          }) %>% 
+          t() %>% 
+          data.frame()
         
         dimnames(scores1)[[1]] <- IDs
         
@@ -96,33 +107,33 @@ CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproporti
             
         vec <- sapply(1:nloci, function(locus){ 
           
-          sort.scores.df[id, locus]==sort.scores.df[id+1,locus]
+          sort.scores.df[id, locus]==sort.scores.df[id+1, locus]
           
         });
         
-        ifelse(sum(!is.na(vec)),sum(vec[!is.na(vec)])/sum(!is.na(vec)),0 )
+        ifelse(sum(!is.na(vec)), sum(vec[!is.na(vec)])/sum(!is.na(vec)), 0 )
         
       }))
       
-      dupIND <- as.vector(duplication>minproportion)
+      dupIND <- as.vector(duplication > minproportion)
       
       if(sum(dupIND)){
         
-        dups <- t(sapply((1:(n-1))[dupIND], function(id){c(ID1 = sortIDs[id], ID2 = sortIDs[id+1])}))
+        dups <- sapply((1:(n-1))[dupIND], function(id){c(ID1 = sortIDs[id], ID2 = sortIDs[id+1])}) %>% t()
         
-        missing <- t(sapply(1:nrow(dups),function(dup){
+        missing <- t(sapply(1:nrow(dups), function(dup){
           
           vec <- match(dups[dup,], sortIDs);
           
-          c("Missing1" = sum(is.na(sort.scores.df[vec[1],])), "Missing2" = sum(is.na(sort.scores.df[vec[2], ])))
+          c("Missing1" = sum(is.na(sort.scores.df[vec[1], ])), "Missing2" = sum(is.na(sort.scores.df[vec[2], ])))
           
         }, simplify = TRUE))
         
         report <- dplyr::bind_cols(dplyr::as_tibble(dups), 
                                   dplyr::as_tibble(missing), 
                                   proportion = duplication[dupIND]
-        ) %>% mutate(silly = silly) %>% 
-          select(silly, tidyr::everything())
+        ) %>% dplyr::mutate(silly = silly) %>% 
+          dplyr::select(silly, tidyr::everything())
         
       }
       
@@ -138,69 +149,133 @@ CheckDupWithinSilly.GCL <- function(sillyvec, loci, quantile = 0.99, minproporti
       
     } #End multicore loop
     
-    parallel::stopCluster(cl)
+    doParallel::stopCluster(cl)
     
-    output %>% 
-      filter(!is.na(silly))
+    output <- dupcheck %>% 
+      dplyr::bind_rows()
+    
+    Sys.time()-start.time
     
     return(output)
     
-  }#End NULL quantile
+  }#End if NULL quantile
   
-  # Start quantile 
-
+  # Start if quantile 
   if(!is.null(quantile)){
-    for(silly in sillyvec){
-      my.gcl=get(paste(silly,".gcl",sep=""),pos=1)
-      scores=my.gcl$scores[,loci,]
-      mydim=dim(scores)
-      if(length(mydim)>2){
-        ID=as.character(dimnames(scores)[[1]])
-        n=length(ID)
-      }
-      if(length(mydim)==2){
-        ID=as.character(my.gcl$attributes$FK_FISH_ID)
-        n=length(ID)
-      }
-      if(length(mydim)<2){
-        ID=NULL
-        n=0
-      }
-     
-      if(n<2){resultlist[[silly]]=list(report="No Duplicates", DupDist=NULL) ; next()}
- 
-
-      ncombs=choose(n,2)        
-
-      mycombs=combn(ID,2)
-
-      scores.df=data.frame(t(sapply(ID,function(id){sapply(loci,function(locus){ifelse(is.na(scores[id,locus,1]),NA,paste(scores[id,locus,1:ploidy[locus]],collapse="."))})})))
-
+    
+    cl <- doParallel::makePSOCKcluster(ncores)
+    
+    doParallel::registerDoParallel(cl, cores = ncores)  
+    
+    # multicore loop
+    dupcheck <- foreach::foreach(silly = sillyvec) %dopar% {
       
-      duplication=sapply(1:ncombs,function(comb){compair=scores.df[mycombs[1,comb],]==scores.df[mycombs[2,comb],];sum(compair[!is.na(compair)])/sum(!is.na(compair))})
+      new.gcl <- my.gcl[[silly]]
+      
+      IDs <- new.gcl$FK_FISH_ID
+      
+      n <- silly_n.GCL("new")$n
+      
+      nloci <- length(loci)
+      
+      #Combine dose 1 and 2 into single columns for each locus separated by a period.
+      scores0 <- new.gcl[ ,c("FK_FISH_ID", scores_cols)] 
+      
+      if(n < 2){
+        
+        report <- tibble::tibble(silly = silly, ID1 = NA, ID2 = NA, Missing1 = NA, Missing2 = NA, proportion = NA) #If only one individual the results will be a tibble of NAs. Can't do next() using applys.
+        
+      } else{
+
+      ncombs <- choose(n, 2)        
+
+      mycombs <- combn(IDs, 2)
+
+      scores1 <- sapply(IDs, function(id){
+        
+        ind <- scores0 %>% 
+          filter(FK_FISH_ID==id)
+        
+        sapply(loci, function(locus){
+          
+          ifelse(is.na(ind[ ,locus]), NA, paste(ind[ , locus], ind[ , paste0(locus,".1")], sep = "."))
+          
+        })
+        
+      }) %>% t() %>% 
+        data.frame () 
+      
+      dimnames(scores1)[[1]] <- IDs
+      
+      duplication <- sapply(1:ncombs, function(comb){
+        
+        compair <- scores1[as.character(mycombs[1, comb]), ] == scores1[as.character(mycombs[2, comb]), ];
+        
+        sum(compair[!is.na(compair)])/sum(!is.na(compair))
+        
+        }) %>% purrr::set_names(sapply(1:ncombs, function(comb){
+          
+          paste(mycombs[,comb], collapse=".")
+          
+          }))
  
-      names(duplication)=sapply(1:ncombs,function(comb){paste(mycombs[,comb],collapse=".")})
+      proportion <- max(quantile(duplication, quantile), minproportion)
  
-      proportion=max(quantile(duplication,quantile),minproportion)
- 
-      dupIND=duplication>proportion
+      dupIND <- duplication > proportion
     
       if(sum(dupIND)){
 
-        dups=data.frame(ID1=mycombs[1,dupIND],ID2=mycombs[2,dupIND])
-  
-        report=data.frame(dups,t(sapply(seq(nrow(dups)),function(row){df=data.frame(Missing1=sum(is.na(scores.df[as.character(dups[row,1]),]))/nloci,Missing2=sum(is.na(scores.df[as.character(dups[row,2]),]))/nloci);vec=scores.df[as.character(dups[row,1]),]==scores.df[as.character(dups[row,2]),];data.frame(df,proportion=sum(vec[!is.na(vec)])/sum(!is.na(vec)))}) ))
- 
+        dups <- data.frame(ID1 = mycombs[1, dupIND], ID2 = mycombs[2, dupIND])
+        
+        missing <- t(sapply(1:nrow(dups),function(dup){
+          
+          vec <- match(as_vector(dups[dup, ]), IDs);
+          
+          c("Missing1" = sum(is.na(scores1[vec[1], ])), "Missing2" = sum(is.na(scores1[vec[2], ])))
+          
+        }, simplify = TRUE))
+        
+        report <- dplyr::bind_cols(dplyr::as_tibble(dups), 
+                                   dplyr::as_tibble(missing), 
+                                   proportion = duplication[dupIND])%>% mutate(silly = silly) %>% 
+          select(silly, tidyr::everything())
+                                   
       }
       if(!sum(dupIND)){
   
-        report="No Duplicates"
+        report <- tibble::tibble(silly = silly, ID1 = NA, ID2 = NA, Missing1 = NA, Missing2 = NA, proportion = NA) 
 
       }
+      
+      }
+      
+      DupDist <- tibble::tibble(silly = silly, IDs = names(duplication), duplication) %>% 
+        separate(IDs, into = c("ID1", "ID2"), sep = "[:punct:]")
 
-        resultlist[[silly]]=list(report=report,DupDist=duplication)  
-    }
+      list(report = report, DupDist = DupDist)  
+       
+    }#End multicore loop
+    
+    parallel::stopCluster(cl)
+  
+    report <- lapply(1:length(sillyvec), function(i){
+      
+      dupcheck[[i]][["report"]]
+      
+      }) %>% bind_rows()
+    
+    DupDist <- lapply(1:length(sillyvec), function(i){
+      
+      dupcheck[[i]][["DupDist"]]
+      
+    }) %>% bind_rows()
+    
+    output <- list(report = report, DupDist = DupDist)
+    
+    Sys.time()-start.time
+    
+    return(output)
 
-    return(resultlist)
-  }#end if
+  }#end if quantile
+    
 }
