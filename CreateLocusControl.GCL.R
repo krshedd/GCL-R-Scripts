94baad7ed9c0555b2b3345ce938f3a5d21f036d2
krshedd <kyle.shedd@alaska.gov>
2020-05-20 14:36:54 -0800

    Minor changes to CreateLocusControl Standardize indentation with Ctrl + i, change lapply with setnames to sapply with simplify = FALSE

--- CreateLocusControl.GCL.R
+++ CreateLocusControl.GCL.R
@@ -46,18 +46,18 @@ CreateLocusControl.GCL <- function(markersuite = NULL, locusnames = NULL, userna
     dir <- path.expand("~/R")
     
     dir.create(dir)
+    
+    bool <- file.copy(from ="V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar", to = path.expand("~/R/ojdbc8.jar"))
+    
+  } else {
+    
+    if(!file.exists(path.expand("~/R/ojdbc8.jar"))){
       
-    bool <- file.copy(from="V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar", to = path.expand("~/R/ojdbc8.jar"))
-      
-    } else{
-      
-      if(!file.exists(path.expand("~/R/ojdbc8.jar"))){
-        
-        bool <- file.copy(from = "V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar", to = path.expand("~/R/ojdbc8.jar"))
-        
-       }
+      bool <- file.copy(from = "V:/Analysis/R files/OJDBC_Jar/ojdbc8.jar", to = path.expand("~/R/ojdbc8.jar"))
       
     }
+    
+  }
   
   options(java.parameters = "-Xmx10g")
   
@@ -65,88 +65,86 @@ CreateLocusControl.GCL <- function(markersuite = NULL, locusnames = NULL, userna
     
     drv <- RJDBC::JDBC("oracle.jdbc.OracleDriver", classPath = "C:/Program Files/R/RequiredLibraries/ojdbc8.jar", " ")#https://blogs.oracle.com/R/entry/r_to_oracle_database_connectivity    C:/app/awbarclay/product/11.1.0/db_1/jdbc/lib
     
-    } else {
-      
-      drv <- RJDBC::JDBC("oracle.jdbc.OracleDriver", classPath = path.expand("~/R/ojdbc8.jar"), " ")
-      
-      } 
+  } else {
     
+    drv <- RJDBC::JDBC("oracle.jdbc.OracleDriver", classPath = path.expand("~/R/ojdbc8.jar"), " ")
+    
+  } 
+  
   url <- LOKI_URL.GCL() #This is a function that gets the correct URL to access the database on the oracle cloud
-    
-  con <- RJDBC::dbConnect(drv,url=url,user=username,password=password)
+  
+  con <- RJDBC::dbConnect(drv, url = url, user = username, password = password)
   
   # Query by 'markersuite', else query by 'locusnames'
   
   if(is.null(markersuite) & is.null(locusnames)) {stop("Need to provide either 'locusnames' or 'markersuite'")}
   
   if(is.null(locusnames) & !is.null(markersuite)) {
-      
+    
     lociqry <- paste("SELECT * FROM AKFINADM.V_LOCUSQRY WHERE SUITE_NAME = '", markersuite, "'", sep = "")  # Query locus information of markers in markersuite.
-      
+    
     locidata <- RJDBC::dbGetQuery(con, lociqry)
-      
+    
     locusnames <- sort(locidata$LOCUS_NAME)
     
-    } else {
-      
-      markersuite <- "User defined from locusnames"
-      
-      lociqry <- paste("SELECT * FROM AKFINADM.V_LOCUSQRY WHERE LOCUS_NAME IN (",paste0("'", locusnames, "'", collapse = ","), ")", sep = "")  # Query locus information of markers in locusnames.
-      
-      locidata <- RJDBC::dbGetQuery(con, lociqry)
-      
-      }
+  } else {
+    
+    markersuite <- "User defined from locusnames"
+    
+    lociqry <- paste("SELECT * FROM AKFINADM.V_LOCUSQRY WHERE LOCUS_NAME IN (",paste0("'", locusnames, "'", collapse = ","), ")", sep = "")  # Query locus information of markers in locusnames.
+    
+    locidata <- RJDBC::dbGetQuery(con, lociqry)
+    
+  }
   
   # Warn user if some 'locusnames' not found
   if(!all(locusnames %in% locidata$LOCUS_NAME)) {
     
     miss_loci <- locusnames[!locusnames %in% locidata$LOCUS_NAME]
-      
+    
     nmiss_loci <- length(miss_loci)
-      
+    
     message(paste(nmiss_loci, "out of", length(locusnames), "locusnames not found in LOKI!!!"))
     
     sapply(miss_loci, function(locus) {
       
       message(locus)
       
-      })
+    })
     
     locusnames <- locusnames[locusnames %in% locidata$LOCUS_NAME]
     
-    }
+  }
   
   Publishedlocusnames <- purrr::set_names(locidata$PUBLISHED_NAME, locidata$LOCUS_NAME)[locusnames]
-    
+  
   nloci <- length(locusnames)
-    
+  
   ploidy <- purrr::set_names(locidata$PLOIDY, locidata$LOCUS_NAME)[locusnames]
-    
-  alleleqry <- paste("SELECT * FROM AKFINADM.V_ALLELEQRY WHERE LOCUS IN (", paste0("'", locusnames, "'", collapse = ","), ")", sep = "")#Get possible alleles from allele lookup table.
-
+  
+  alleleqry <- paste("SELECT * FROM AKFINADM.V_ALLELEQRY WHERE LOCUS IN (", paste0("'", locusnames, "'", collapse = ","), ")", sep = "")  # Get possible alleles from allele lookup table.
+  
   alleles0 <- RJDBC::dbGetQuery(con, alleleqry)
-
+  
   discon <- RJDBC::dbDisconnect(con) #Disconnect from database
   
-  alleles <- lapply(locusnames, function(locus){
-  
+  alleles <- sapply(locusnames, function(locus){
+    
     my.loc = dplyr::filter(alleles0, LOCUS==locus) %>% 
       dplyr::select(-VALUE_CTC)
-   
+    
     nalleles = dim(my.loc)[1] 
     
     tibble::tibble(allele = seq(nalleles), call = my.loc$VALUE %>% sort())
     
-  }) %>% purrr::set_names(locusnames)
-    
+  }, simplify = TRUE)
+  
   nalleles <- sapply(alleles, dim)[1,] 
-      
+  
   assign("LocusControl", tibble::tibble(MarkerSuite = markersuite, locusnames = locusnames, Publishedlocusnames = Publishedlocusnames, nalleles = nalleles, ploidy = ploidy, alleles = alleles), pos = 1, envir = .GlobalEnv) #Assign elements to LocusControl list.	
-
+  
   ans <- paste0("LocusControl created for markersuite ='", markersuite, "'")
-    
+  
   return(ans)
-
-}  
-   
-
+  
+}
