5b89f146a41622ec88d929648bec60b6660cfd64
Barclay <andy.barclay@dfg.alaska.local>
2020-06-02 10:38:47 -0800

    RemoveDups - initial commit

--- RemoveDups.GCL.r
+++ RemoveDups.GCL.r
@@ -1,31 +1,62 @@
-RemoveDups.GCL=function(dupcheck){
-##################################################################################################################################################################
-#
-#  Removes duplicated "IDs" with the highest proportion of missingness.
-#
-#  "dupcheck" is an object created by the function "CheckDupWithinSilly.GCL".
-#
-##################################################################################################################################################################
+RemoveDups.GCL <- function(dupcheck){
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  This function removes duplicated "IDs" with the highest number of missing loci. 
+  #  If both IDs have the same number of missing loci, ID1 is removed.
+  #
+  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   
+  #   dupcheck - an object created by the function "CheckDupWithinSilly.GCL". 
+  #
+  # Output~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #
+  #  This function returns a tibble of IDs removed for each silly in dupcheck.
+  #
+  # Examples~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  CreateLocusControl.GCL(markersuite = "Sockeye2011_96SNPs", username = "awbarclay", password = password)
+  #  sillyvec = c("SMCDO03", "SNEVA13")
+  #  password = "************"
+  #  LOKI2R.GCL(sillyvec = sillyvec, username = "awbarclay", password = password)
+  #  RemoveIndMissLoci.GCL(sillyvec = sillyvec)
+  #
+  #  dupcheck <- CheckDupWithinSilly.GCL(sillyvec = sillyvec, loci = LocusControl$locusnames, quantile = NULL, minproportion = 0.95, ncores = 8)
+  #  removed_dups <- RemoveDups.GCL(dupcheck)
+  #
+  # Note~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   When quantile is set to NULL this function utilizes rubias::close_matching_samples() to perform the duplicate check and it much faster than when you set a quantile.
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse)  # Install packages, if not in library and then load them.
 
-  allsillys=names(dupcheck)
+  if(!tibble::is_tibble(dupcheck)){dupcheck <- dupcheck$report}
+  
+  dupcheck_names <-  c("silly", "ID1", "ID2", "Missing1", "Missing2", "proportion")
+  
+  if(length(setdiff(dupcheck_names, names(dupcheck))) > 0){
+    
+    stop(paste0("Nothing removed. Dupcheck must contain the folling variables: ", paste0(dupcheck_names, collapse = ", ")))
+    
+    }
+  
+  sillys <- dupcheck$silly %>% 
+    unique()
+  
+  to_remove <- dupcheck %>%
+    mutate(remove = case_when(Missing1 > Missing2~ID1,
+                              Missing2 > Missing1~ID2,
+                              Missing1 == Missing2~ID1))
+  
+  results <- lapply(sillys, function(silly){
+    
+    remove <- to_remove %>% 
+      filter(silly==!!silly) %>% 
+      select(silly, removed_IDs = remove)
+    
+    RemoveIDs.GCL(silly = silly, IDs = remove$removed_IDs)
+    
+    remove
+    
+  }) %>% bind_rows()
 
-  dupcheck=lapply(allsillys,function(silly){dupcheck[[silly]]$report})
-  names(dupcheck)=allsillys
-
-  results=vector("list",length(allsillys))
-  names(results)=allsillys
-
-  results[1:length(allsillys)]="Nothing Removed"
-
-  sillys=allsillys[sapply(dupcheck,function(dc){is.data.frame(dc)})]
-
-  if(!length(sillys)){return(results)}
-
-  for(silly in sillys){
-    IND=apply(dupcheck[[silly]][,c("Missing1","Missing2")],1,function(x){order(as.numeric(x),decreasing=TRUE)[1]})
-    remove=as.character(unique(unlist(sapply(seq(nrow(dupcheck[[silly]][,c("ID1","ID2")])),function(i){dupcheck[[silly]][i,c("ID1","ID2")][IND[i]]}))))
-    RemoveIDs.GCL(silly=silly,IDs=remove)
-    results[[silly]]=remove
-  }
   return(results)
 }
