dc8206ae326fc954ec102c57690ff8edf53f9139
Barclay <andy.barclay@dfg.alaska.local>
2020-06-02 10:30:50 -0800

    RemoveIDs - initial commit

--- RemoveIDs.GCL.r
+++ RemoveIDs.GCL.r
@@ -1,36 +1,41 @@
-RemoveIDs.GCL=function(silly,IDs){
-##################################################################################################################################################################
-#
-#  Removes "IDs" (vial #'s as oppossed to positions) from the "*.gcl" object associated with "silly".
-#
-##################################################################################################################################################################
-
-  if(!is.list(IDs)){
-    IDs=list(IDs)  
+RemoveIDs.GCL <- function(silly, IDs){
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   This function removes "IDs" (FK_FISH_ID) from the "*.gcl" object associated with "silly".
+  #
+  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   
+  #   silly - a silly code without the ".gcl" extention.
+  #
+  #   IDs - a numeric or character vector of FK_FISH_ID's to remove from "silly".
+  #
+  # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  
+  #  Assigns "*.gcl" objects with removed IDs to the current workspace. 
+  #
+  # Examples~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  CreateLocusControl.GCL(markersuite = "Sockeye2011_96SNPs", username = "awbarclay", password = password)
+  #  sillyvec = c("SMCDO03", "SNEVA13")
+  #  password = "************"
+  #  LOKI2R.GCL(sillyvec = sillyvec, username = "awbarclay", password = password)
+  #  RemoveIndMissLoci.GCL(sillyvec = sillyvec)
+  #
+  #  RemoveIDs.GCL(silly = "SMCDO03", IDs = 1:10)
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse)  # Install packages, if not in library and then load them.
+  
+  my.gcl <- get(paste(silly,".gcl",sep=""), pos = 1)
+  
+  if(purrr::is_character(IDs)){IDs <- as.numeric(IDs)}
+  
+  if(sum(my.gcl$FK_FISH_ID%in%IDs) < length(IDs)){
+    
+    stop(paste0("No IDs were removed. Some of the IDs do not exist for ", silly, ". "))
+    
   }
-  names(IDs)=silly
 
-  my.gcl=get(paste(silly,".gcl",sep=""),pos=1)
-
-  scores=my.gcl$scores
-
-  counts=my.gcl$counts
+  assign(paste(silly,".gcl",sep=""), my.gcl %>% dplyr::filter(!FK_FISH_ID%in%IDs), pos = 1)
  
-  attributes=my.gcl$attributes
-
-  vials=as.character(dimnames(scores)[[1]])
-
-  IND=!vials%in%IDs[[silly]]
-
-  counts=counts[IND,,,drop=FALSE]
-
-  scores=scores[IND,,,drop=FALSE]
-
-  n=nrow(scores)
-
-  attributes=attributes[IND,,drop=FALSE]
-
-  assign(paste(silly,".gcl",sep=""),list(counts=counts,scores=scores,n=n,attributes=attributes),pos=1)
-
-  return(NULL)
+  warning(paste0(length(IDs), " IDs were removed from ", silly, ".gcl\n"))
+  
 }
