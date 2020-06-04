408d326bf5b20fd2444fbd169fe0f0ef1a814a17
Barclay <andy.barclay@dfg.alaska.local>
2020-06-04 14:53:36 -0800

    FreqPop - completely rewritten to work with new tidy ".gcl" objects.  Need to test this more to see if it will break.

--- FreqPop.GCL.r
+++ FreqPop.GCL.r
@@ -1,36 +1,91 @@
-FreqPop.GCL=function(sillyvec,loci){
+FreqPop.GCL <- function(sillyvec, loci, ncores = 4 ){
+ 
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   This function gets the allele frequency for each locus in loci for each silly in sillyvec.
+  #
+  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   
+  #   sillyvec - a vector of silly codes without the ".gcl" extention (e.g. sillyvec <- c("KQUART06","KQUART08","KQUART10")). 
+  #
+  #   loci - vector of locus names; if set to NULL all loci in the ".gcl" obejects will be used.
+  #
+  #   ncores - the number of cores to use in a foreach %dopar% loop. If the nubmer of core exceeds the number on your device, then ncores defaults to detectCores()
+  # 
+  # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  A tibble with the following variables: silly, locus, allele_no (from LocusControl), freq (allele frequency), and proportion
+  #
+  # Examples~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #  load("V:/Analysis/2_Central/Chinook/Cook Inlet/2019/2019_UCI_Chinook_baseline_hap_data/2019_UCI_Chinook_baseline_hap_data.RData")
+  #  old2new_LocCtrl.GCL()  
+  #  old2new_gcl.GCL(sillyvec67)
+  #  Freq <- FreqPop.GCL(sillyvec = sillyvec67, loci = loci413)
+  #
+  # Note~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   When quantile is set to NULL this function utilizes rubias::close_matching_samples() to perform the duplicate check and it much faster than when you set a quantile.
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-##############################################################################################################################################################################
-#
-#  This function creates an array of allele frequencies
-#
-# Written by JJ 6/3/2011
-##############################################################################################################################################################################
-
-  if(sum(is.na(match(loci,LocusControl$locusnames)))){stop(paste("'",loci[is.na(match(loci,LocusControl$locusnames))],"' from argument 'loci' not found in 'LocusControl' object!!!",sep=""))}
-
-  nsilly=length(sillyvec)
-
-  nloci=length(loci)
-
-  alleles=LocusControl$alleles[loci]
-
-  nalleles=LocusControl$nalleles[loci]
-
-  ploidy=LocusControl$ploidy[loci]
-
-  Freq=array(NA,c(nsilly,nloci,max(nalleles)),dimnames=list(sillyvec,loci,paste("Allele ",1:max(nalleles),sep="")))
+  start.time <- Sys.time() 
   
-  for(silly in sillyvec){
-    my.gcl=get(paste(silly,".gcl",sep=""),pos=1)
-    scores=my.gcl$scores
-    for(locus in loci){
-      myscores=scores[,locus,1:ploidy[locus]][!is.na(scores[,locus,1:ploidy[locus]])]
-      allelevec=paste("Allele ",1:nalleles[locus],sep="")
-      for(allele in 1:nalleles[locus]){
-        Freq[silly,locus,allelevec[allele]]=sum(myscores==alleles[[locus]][allele])
-      }
-    }
+  if(sum(is.na(match(loci, LocusControl$locusnames)))){
+    
+    stop(paste("'", loci[is.na(match(loci,LocusControl$locusnames))], "' from argument 'loci' not found in 'LocusControl' object!!!", sep = ""))
+    
   }
-  return(Freq)
+  
+  if(ncores > detectCores()){
+    
+    stop("'ncores' is greater than the number of cores available on machine\nUse 'detectCores()' to determine the number of cores on your machine")
+  
+    }
+  
+  if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse, doParallel, parallel, foreach)  # Install packages, if not in library and then load them
+
+  all.gcl <- lapply(sillyvec, function(silly){get(paste(silly, ".gcl", sep = ""), pos = 1)}) %>% 
+    purrr::set_names(sillyvec)
+  
+  scores_cols <- c(loci, paste0(loci, ".1")) %>% 
+    sort()
+  
+  alleles <- LocusControl$alleles[loci] %>% 
+    dplyr::bind_rows(.id = "locus") %>% 
+    dplyr::rename(allele_no = allele)
+  
+  cl <- parallel::makePSOCKcluster(ncores)
+  
+  doParallel::registerDoParallel(cl, cores = ncores)  
+
+  #Start parallel loop
+  freqs <- foreach::foreach(silly = sillyvec, .packages = c("tidyverse")) %dopar% {
+    
+    my.gcl <- all.gcl[[silly]]
+    
+    my.gcl %>% 
+      dplyr::select(all_of(scores_cols)) %>% 
+      dplyr::gather(key = "locus", value = "allele") %>% 
+      dplyr::filter(locus%in%loci) %>%
+      dplyr::full_join(alleles, by = c("locus"="locus", "allele"="call"), keep = TRUE) %>% 
+      dplyr::mutate(locus = gsub("\\.1$", "", locus.y)) %>% 
+      dplyr::group_by(locus, allele_no, call) %>% 
+      dplyr::summarize(freq = sum(!is.na(allele)), .groups = "drop_last") %>% 
+      dplyr::mutate(silly = !!silly) %>% 
+      dplyr::filter(!is.na(allele_no))
+   
+  } %>% 
+    dplyr::bind_rows() %>% 
+    dplyr::relocate(silly, before = locus)
+  
+  parallel::stopCluster(cl) #End parallel loop
+  
+  output <- freqs %>% 
+    dplyr::rename(allele = call) %>% 
+    dplyr::group_by(silly, locus) %>% 
+    dplyr::mutate(total = sum(freq)) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::mutate(proportion = freq/total) %>% 
+    dplyr::select(-total)
+  
+  print(Sys.time()-start.time)
+  
+  return(output)
+  
 }
