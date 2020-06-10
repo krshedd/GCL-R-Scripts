54c92279a14c053a7284731cbb29ad4357ef5e13
Barclay <andy.barclay@dfg.alaska.local>
2020-06-10 09:14:00 -0800

    FisherCompute - added documentation and other minor fixes.

--- FisherCompute.GCL.r
+++ FisherCompute.GCL.r
@@ -1,4 +1,32 @@
 FisherCompute.GCL <- function(freq, loci, prec = 4){
+  
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   This function is a wrapper for stats::fisher.test() and runs a Fisher's Exact Test for homogeneity of allele frequencies.
+  #   Null hypothesis: there are no significant differences in allele freqencies.
+  #
+  # Inputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   
+  #   freq - a tibble of allele freqencies produced by FreqPop.GCL() for the collections you want to test.
+  #
+  #   loci - a character vector of locus names
+  #   
+  #   prec - the precision of the output pvalues (i.e., the number of significant digits)
+  # 
+  # Outputs~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #    Produces a tibble of overall pvalues produced by combining individual locus pvalues using Fisher's method and a nested tibble of pvalues by locus.
+  # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #   load("V:/Analysis/2_Central/Chinook/Cook Inlet/2019/2019_UCI_Chinook_baseline_hap_data/2019_UCI_Chinook_baseline_hap_data.RData")
+  #   old2new_LocCtrl.GCL()
+  #   old2new_gcl.GCL(sillyvec = c("KKILL05","KKILL06"), save_old = FALSE)
+  #
+  #   freq <- FreqPop.GCL(sillyvec = c("KKILL05","KKILL06"), loci = loci443)
+  # 
+  #   FisherCompute.GCL(freq = freq, loci = loci443, prec = 4)
+  # 
+  # Note~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # This function is called on by FishersTest.GCL to do multiple tests at once.
+  #
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	
   if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse) #Install packages, if not in library and then load them.
 
@@ -11,21 +39,21 @@ FisherCompute.GCL <- function(freq, loci, prec = 4){
    
    freq.mat <- matrix(my.freq$freq, ncol = max(my.freq$allele_no), dimnames = list(unique(my.freq$silly), unique(my.freq$allele_no)), byrow = TRUE)
    
-   fisher.test(freq.mat, workspace = 8000000, hybrid = TRUE)$p.value
+   suppressWarnings(fisher.test(freq.mat, workspace = 8000000, hybrid = TRUE))$p.value
     
   })
   
   pval[pval==0] = min(pval[pval!=0])#I think this is something Jim added so you can calculate overall p-value. I'm not sure why he decided to use the minimum pvalue to replace zeros.
   
   #Using Fisher's method to get overall pvalue
-  overall <- pchisq(q =- 2*sum(log(pval)), df = 2*nloci, lower.tail = FALSE) %>% 
+  overall <- pchisq(q =- 2*sum(log(pval)), df = 2*length(loci), lower.tail = FALSE) %>% 
     round(prec)
   
   bylocus <- tibble::tibble(locus = names(pval), pval = pval %>% round(prec))
   
   test <- paste0(unique(freq$silly), collapse = ".")
   
-  output <- tibble::tibble(silly = test,  overall = !!overall, bylocus = !!bylocus %>% tidyr::nest({{test}} := dplyr::everything()))
+  output <- tibble::tibble(test_sillys = test,  overall = !!overall, bylocus = !!bylocus %>% tidyr::nest({{test}} := dplyr::everything()))
 
   return(output)
   
