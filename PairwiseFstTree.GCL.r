2c55ae4e231d2382ce261c466f32bf17a336efee
Barclay <andy.barclay@dfg.alaska.local>
2020-06-11 15:17:46 -0800

    PairwiseFstTree - made code more readable by adding spaces, replaced write fstat code with gcl2FSTAT(), and added package names:: where they were needed

--- PairwiseFstTree.GCL.r
+++ PairwiseFstTree.GCL.r
@@ -54,13 +54,13 @@ PairwiseFstTree.GCL <- function(sillyvec, loci, dir, nboots = 1000, ncores = 4, 
     
   }
   
-  if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse, ape, foreach, doParallel, parallel) #Install packages, if not in library and then load them.
+  if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse, ape, foreach, doParallel, parallel, hierfstat) #Install packages, if not in library and then load them.
   
   if(ncores > detectCores()) {
     
     stop("'ncores' is greater than the number of cores available on machine\nUse 'detectCores()' to determine the number of cores on your machine")
   
-    }
+  }
   
   message("\n4 Main function tasks: \n1) Create fstat .dat file\n2) Calculate variance componenets for each pair of sillys\n3) Calculate bootstrap Fst values\n4) Bootstrap tree nodes\n")
   
@@ -68,164 +68,166 @@ PairwiseFstTree.GCL <- function(sillyvec, loci, dir, nboots = 1000, ncores = 4, 
   
   start.time <- Sys.time() 
   
-  fstatdir <- paste(dir,"\\fstatfile.dat",sep="")  
+  fstatdir <- paste0(dir,"/fstat.dat")
   
-  cl=makePSOCKcluster(ncores)
+  n <- silly_n.GCL(sillyvec) %>% 
+    pull(n) %>% 
+    purrr::set_names(sillyvec)
   
-  registerDoParallel(cl,cores=ncores)  
+  nsillys <- length(sillyvec)
   
-  nsillys=length(sillyvec)
+  nloci <- length(loci)
+  
+  gcl2FSTAT.GCL(sillyvec = sillyvec, loci = loci, path = fstatdir, ncores = ncores)
+  
+  dat <- hierfstat::read.fstat.data(fstatdir, na.s = c("0", "00", "000", "0000", "00000", "000000", "NA", "NANA"))
 
-  maxsillychar=nchar(nsillys)+1
+  pairs <- combn(sillyvec,2)
 
-  nloci=length(loci)
-
-  ploidy=LocusControl$ploidy[loci]
-
-  nalleles=LocusControl$nalleles[loci]
-
-  maxchar=nchar(nalleles)+1
-  names(maxchar)=loci
-
-  alleles=LocusControl$alleles[loci]
-
-  my.gcl=lapply(sillyvec,function(silly){get(paste(silly,".gcl",sep=""),pos=1)})
-  names(my.gcl)=sillyvec
-
-  n=sapply(sillyvec,function(silly){my.gcl[[silly]]$n})
-  names(n)=sillyvec
-
-  scores=lapply(sillyvec,function(silly){scores0 <- my.gcl[[silly]]$scores[,loci,] ; scores0[scores0 %in% c("Unk", "XXX", "0")] <- NA ; scores0})
-  names(scores)=sillyvec
-
-  counts <- foreach(silly=sillyvec) %dopar% {
-    sapply(1:n[silly],function(i){
-      paste(c(match(silly,sillyvec),sapply(loci,function(locus){
-        ifelse(is.na(scores[[silly]][i,locus,1]),paste(rep(0,ploidy[locus]*maxchar[locus]),collapse=""),paste(sapply(1:ploidy[locus],function(allele){
-          paste(c(rep(0,maxchar[locus]-nchar(match(scores[[silly]][i,locus,allele],alleles[[locus]]))),match(scores[[silly]][i,locus,allele],alleles[[locus]])),collapse="")
-        }),collapse=""))
-      })),collapse=" ")
+  pairnames <- apply(pairs, 2, function(col){
+    
+    paste(col, collapse = ".")
+    
     })
-  }
-  names(counts)=sillyvec
-
-  fstat=paste(nsillys,nloci,max(nalleles),max(maxchar),sep=" ")
-
-  fstat=rbind(fstat,cbind(loci))
-
-  fstat=rbind(fstat,cbind(as.vector(unlist(counts))))
-
-  write.table(fstat,fstatdir,row.names=FALSE,col.names=FALSE,quote=FALSE) 
   
-  message("\nfstat file created\n", sep = '')
+  dimnames(pairs)[[2]] <- pairnames
   
-  dat=read.fstat.data(fstatdir, na.s = c("0", "00", "000", "0000", "00000", "000000", "NA", "NANA"))
-
-  pairs=combn(sillyvec,2)
-
-  pairnames=apply(pairs,2,function(col){paste(col,collapse=".")})
-  
-  dimnames(pairs)[[2]]=pairnames
+  ploidy <- LocusControl$ploidy
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   # Calculate variance components for each pair of sillys
   message("\nCalculate variance components for each pair of sillys\n", sep = '')
 
-  vc <- foreach(pair=pairnames, .packages = "hierfstat") %dopar% {
-    sillys=pairs[,pair]
-    sillyIND=sapply(sillys,function(silly){match(silly,sillyvec)})
-    mydat=dat[dat[,1]==sillyIND[1] | dat[,1]==sillyIND[2],loci]
-    myn=n[sillys]
-    levels=rep(1:2,myn)
+  vc <- foreach::foreach(pair = pairnames, .packages = "hierfstat") %dopar% {
+    
+    sillys <- pairs[, pair]
+    
+    sillyIND <- sapply(sillys, function(silly){match(silly, sillyvec)})
+    
+    mydat <- dat[dat[, 1]==sillyIND[1] | dat[, 1]==sillyIND[2], loci]
+    
+    myn <- n[sillys]
+    
+    levels <- rep(1:2, myn)
     
     mito.loci <- which(ploidy[loci] == 1)
     
     # Create matrix of variance components with order loci
     if(length(mito.loci) == 0) {
-      vc.pair <- 
-        # Diploid loci
-        t(sapply(loci, function(locus) {
-          mydata = data.frame(levels,mydat[,locus])
-          hierfstat::varcomp(data = data.matrix(mydata), diploid=TRUE)$overall
-        } ))
-    } else {
-      vc.pair <- rbind(
+      
       # Diploid loci
-      t(sapply(loci[-mito.loci], function(locus) {
+      vc.pair <- t(sapply(loci, function(locus) {
+        
+        mydata = data.frame(levels, mydat[,locus])
+        
+        hierfstat::varcomp(data = data.matrix(mydata), diploid = TRUE)$overall
+        
+      }))
+      
+    } else {
+      
+      # Diploid loci
+      vc.pair <- rbind(t(sapply(loci[-mito.loci], function(locus){
+        
         mydata = data.frame(levels,mydat[,locus])
         hierfstat::varcomp(data = data.matrix(mydata), diploid=TRUE)$overall
-      } )),
+        
+      })),
       
       # Haploid loci
       t(sapply(loci[mito.loci], function(locus) {
-        mydata = data.frame(levels,mydat[,locus])
-        c(hierfstat::varcomp(data = data.matrix(mydata), diploid=FALSE)$overall,0)
+        
+        mydata <- data.frame(levels,mydat[,locus])
+        
+        c(hierfstat::varcomp(data = data.matrix(mydata), diploid = FALSE)$overall,0)
+        
       } ))
-    )[loci, ]
+      
+      )[loci, ]
+      
     }
     
     # Replace NAs with 0s
     vc.pair[is.na(vc.pair)] <- 0
     vc.pair
+  
   }
+  
   names(vc) <- pairnames
   
   # Calculate Fst from VC for each pair
   Fst.ls <- lapply(vc, function(pair) {
+    
     sum(pair[, 1]) / sum(pair)
+    
   })
   
-  
   # Create matrix of pairwise Fst
-  Fst <- array(0,c(nsillys,nsillys),dimnames=list(sillyvec,sillyvec))
+  Fst <- array(0, c(nsillys, nsillys), dimnames = list(sillyvec, sillyvec))
   
   Fst[lower.tri(Fst)] <- unlist(Fst.ls)
+  
   Fst[upper.tri(Fst)] <- t(Fst)[upper.tri(Fst)]
   
   # Create tree
-  tree=ape::nj(Fst)
+  tree <- ape::nj(Fst)
 
   # Bootstraps
   message("\nCalculate bootstrap Fst values\n", sep = '')
   
-  trees.bootstrapFst <- foreach(i=seq(nboots), .packages = "ape") %dopar% {
+  trees.bootstrapFst <- foreach(i = seq(nboots), .packages = "ape") %dopar% {
     
-    temploci=sample(loci,nloci,replace=TRUE)
+    temploci <- sample(loci, nloci, replace = TRUE)
+    
     tempFst.ls <- lapply(vc, function(pair) {
+      
       sum(pair[temploci, 1]) / sum(pair[temploci, 1:3])
+      
     })
-    tempFst <- array(0,c(nsillys,nsillys),dimnames=list(sillyvec,sillyvec))
+    
+    tempFst <- array(0, c(nsillys, nsillys), dimnames = list(sillyvec, sillyvec))
+    
     tempFst[lower.tri(tempFst)] <- unlist(tempFst.ls)
+    
     tempFst[upper.tri(tempFst)] <- t(Fst)[upper.tri(tempFst)]
     
     list(trees = ape::nj(tempFst), bootstrapFst = tempFst)
+    
   }
 
   bootstrapFst <- lapply(trees.bootstrapFst, function(i) {
+    
     i[["bootstrapFst"]]
+    
   })
   
   trees <- lapply(trees.bootstrapFst, function(i) {
+    
     i[["trees"]]
+    
   })
   
   message("\nBootstrap tree nodes\n", sep = '')
   
-  bootstrap=prop.clades(tree,trees)
+  bootstrap <- ape::prop.clades(tree, trees)
 
   # Create final PairwiseFstTree list object
   if(returnbootstrapFst) {
-    PairwiseFstTree <- list(tree=tree,bootstrap=bootstrap,PairwiseFst=Fst,vc=vc,BootstrapFst=bootstrapFst)
+    
+    PairwiseFstTree <- list(tree = tree, bootstrap = bootstrap, PairwiseFst = Fst, vc = vc, BootstrapFst = bootstrapFst)
+    
   } else {
-    PairwiseFstTree <- list(tree=tree,bootstrap=bootstrap,PairwiseFst=Fst,vc=vc)
+    
+    PairwiseFstTree <- list(tree = tree, bootstrap = bootstrap, PairwiseFst = Fst, vc = vc)
+    
   }
   
   # Stop cluster
-  stopCluster(cl)
+  parallel::stopCluster(cl)
   
   # Save tree before exiting
-  dput(x = PairwiseFstTree, file = paste(dir,"\\", length(sillyvec), "Pops", length(loci), "Loci_","PairwiseFstTree.txt",sep="")  )
+  dput(x = PairwiseFstTree, file = paste(dir,"\\", length(sillyvec), "Pops", length(loci), "Loci_", "PairwiseFstTree.txt", sep = ""))
   
   stop.time <- Sys.time()
   
