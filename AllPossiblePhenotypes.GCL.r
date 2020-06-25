05ad439d54761c0d2b5ac1a7ee5c4c84ea73b328
krshedd <kyle.shedd@alaska.gov>
2020-06-25 13:45:28 -0800

    AllPossiblePhenotypes - minor formatting changes

--- AllPossiblePhenotypes.GCL.r
+++ AllPossiblePhenotypes.GCL.r
@@ -29,58 +29,58 @@ AllPossiblePhenotypes.GCL <- function(markerset){
   newgntps <- lapply(
     
     lapply(markerset, function(marker){
+      
+      t(combn(rep(alleles[[marker]]$call, ploidy[marker]), ploidy[marker]))
+      
+    }), function(gmat){
+      
+      na <- NULL
+      
+      nr <- nrow(gmat)
+      
+      nc <- ncol(gmat)
+      
+      for(i in 1:nr){
+        
+        if(nc > 1){
+          
+          vec <- sort(gmat[i, ])
+          
+        } else{
+          
+          vec <- gmat[i]
+          
+        } 
+        
+        na <- c(na, paste(vec, collapse = ""))
+        
+      }
+      
+      sort(unique(na))
+      
+    })
+  
+  if(length(unique(markerset)) == 1){
     
-    t(combn(rep(alleles[[marker]]$call, ploidy[marker]), ploidy[marker]))
+    newalleles = newgntps[[1]]
+    
+  } else {
+    
+    temp <- expand.grid(newgntps, stringsAsFactors = FALSE)     
+    
+    nr <- nrow(temp)
+    
+    na <- NULL
+    
+    for(i in 1:nr){
       
-      }), function(gmat){
-        
-        na <- NULL
-        
-        nr <- nrow(gmat)
-        
-        nc <- ncol(gmat)
-        
-        for(i in 1:nr){
-          
-          if(nc>1){
-            
-            vec <- sort(gmat[i, ])
-          
-          } else{
-            
-            vec <- gmat[i]
-            
-            } 
-          
-          na <- c(na, paste(vec, collapse = ""))
-          
-          }
-        
-        sort(unique(na))
-        
-        })
-
-     if(length(unique(markerset))==1){
-
-       newalleles = newgntps[[1]]
-
-     }else{
+      na <- c(na, paste(as.character(temp[i, ]), collapse = ""))
       
-       temp <- expand.grid(newgntps, stringsAsFactors = FALSE)     
-
-       nr <- nrow(temp)
-
-       na <- NULL
-
-       for(i in 1:nr){
-
-         na <- c(na, paste(as.character(temp[i, ]), collapse = ""))
-
-       }      
-
-       newalleles <- sort(unique(na))
-
-    }#else 
-
-    return(newalleles)
+    }      
+    
+    newalleles <- sort(unique(na))
+    
+  }  # else 
+  
+  return(newalleles)
 }
