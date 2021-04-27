28040ca22236e0f62ef4e1ae8fd7f82bd9dd2bbf
krshedd <kyle.shedd@alaska.gov>
2021-04-27 15:59:21 -0800

    Bugfix StratifiedEstimator - gotta close them )

--- StratifiedEstimator.GCL.r
+++ StratifiedEstimator.GCL.r
@@ -48,7 +48,7 @@ nstrata=length(mixvec)
 
 if(nstrata==1){stop("This function is for multiple strata!!!")}
   
-if(length(mixvec) != length(catchvec){stop("mixvec and catchvec must be the same length")}
+if(length(mixvec) != length(catchvec)) {stop("mixvec and catchvec must be the same length")}
 
 if(xlxs){
   while(!require(xlsx)){install.packages("xlsx")}
