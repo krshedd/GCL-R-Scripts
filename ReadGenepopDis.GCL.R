697e59092aee5901965f06a4903660757f005a5a
Barclay <andy.barclay@dfg.alaska.local>
2021-07-01 07:34:01 -0800

    ReadGenepopDis.GCL - cleaned up code and updated to R version 4.0

--- ReadGenepopDis.GCL.R
+++ ReadGenepopDis.GCL.R
@@ -1,8 +1,9 @@
 ReadGenepopDis.GCL <- function(file, loci = NULL, sillyvec = NULL) {
 #################################################################################################################
 #
-# This function reads in the output from a GENEPOP disequillibrium ("*.dis") file and returns a tibble with p-value columns for each population
-# and a column of overall population p-values. If the disequillibrium file contains tests for only 1 population, the tibble will not contain on 'Overall' column.
+# This function reads in the output from a GENEPOP disequilibrium ("*.dis") file and returns a tibble with p-value 
+# columns for each population and a column of overall population p-values. If the disequilibrium file contains tests 
+# for only 1 population, the tibble will not contain on 'Overall' column.
 #
 # P-values with "No contingency table" are replaced with "1" and p-values = "0" are replaced with 1/(#Batches*#Iterations per batch).
 # 
@@ -16,10 +17,10 @@ ReadGenepopDis.GCL <- function(file, loci = NULL, sillyvec = NULL) {
 # loci <- loci82
 #   ~ default is NULL or a vector of locus names the same order as the genepop input file used for the tests. 
 # If a vector of locus names is supplied the Locus1 and Locus2 columns will contain the correct locus names
-# otherwise the locus names will be the same as in the .DIS file (tuncated to 8 characters)
+# otherwise the locus names will be the same as in the .DIS file (truncated to 8 characters)
 #
 # sillyvec <- sillyvec94
-#   ~ default is NULL or a vector of population sillys the same order as in the genepop inpufile used for the tests. 
+#   ~ default is NULL or a vector of population sillys the same order as in the genepop input file used for the tests. 
 #     If a vector of silly's is supplied the header names for each pop in the output tibble will be the correct sillys, 
 #     otherwise, if no vector is supplied, the header names will be the SillySource of the last fish in the population.
 #
@@ -37,56 +38,55 @@ ReadGenepopDis.GCL <- function(file, loci = NULL, sillyvec = NULL) {
 # Example~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # How many populations have a p-value of < 0.05 for a given locus pair?
 # Create an additional column with the number of pops below a given p-value threshold (i.e. 0.05)
-# LD <- ReadGenepopDis.GCL(file = "V:/Analysis/3_AYK/Chum/WASC/Genepop/WASCchum.DIS",loci=NULL)
-# LD$npopsfail <- apply(LD[, 3:dim(LD)[2]] < 0.05, 1, function(locuspair) {sum(locuspair)} )
+# LD <- ReadGenepopDis.GCL(file = "V:/Analysis/3_AYK/Chum/WASC/Genepop/WASCchum.DIS",loci = NULL)
+# LD$npopsfail <- apply(LD[ , 3:dim(LD)[2]] < 0.05, 1, function(locuspair) {sum(locuspair)} )
 #
-# Written 4/25/19 Andy Barclay 
 #################################################################################################################
 
   require(tidyverse)
 
-  dis <- scan(file,what='',sep = "\n")
+  dis <- scan(file, what = '', sep = "\n")
   
-  npops <- as.numeric(strsplit(dis[grep("Number of populations detected :",dis)],split="Number of populations detected :")[[1]][2])
+  npops <- as.numeric(strsplit(dis[grep("Number of populations detected :", dis)], split = "Number of populations detected :")[[1]][2])
   
-  nloci <- as.numeric(strsplit(dis[grep("Number of loci detected        :",dis)],split="Number of loci detected        :")[[1]][2])
+  nloci <- as.numeric(strsplit(dis[grep("Number of loci detected        :", dis)], split = "Number of loci detected        :")[[1]][2])
 
-  batches <- as.numeric(strsplit(dis[grep("	Batches              :",dis)],split="\tBatches              : ")[[1]][2])
+  batches <- as.numeric(strsplit(dis[grep("	Batches              :", dis)], split = "\tBatches              : ")[[1]][2])
 
-  iterations <- as.numeric(strsplit(dis[grep("	Iterations per batch : ",dis)],split="\tIterations per batch : ")[[1]][2])
+  iterations <- as.numeric(strsplit(dis[grep("	Iterations per batch : ", dis)], split = "\tIterations per batch : ")[[1]][2])
 
-  repzero <- format(1/(batches*iterations),scientific=F,digits=6)
+  repzero <- format(1/(batches * iterations), scientific = FALSE, digits = 6)
 
-  popstart <- grep("Pop             Locus#1  Locus#2    P-Value      S.E.     Switches",dis)+2
+  popstart <- grep("Pop             Locus#1  Locus#2    P-Value      S.E.     Switches", dis) + 2
   
-  ncomps<-choose(nloci,2)
+  ncomps <- choose(nloci, 2)
   
-  popend <- popstart+ncomps*npops-1
+  popend <- popstart + ncomps * npops - 1
   
   #Loci
   if(!is.null(loci)) {
     
-    loc1 <- sapply(seq(length(loci)-1),function(i){
+    loc1 <- sapply(seq(length(loci)-1), function(i){
       
       loci[1:i]
     
     }) %>% unlist()
     
-    loc2 <- sapply(seq(length(loci)),function(i){
+    loc2 <- sapply(seq(length(loci)), function(i){
       
-      rep(loci[i],i-1)
+      rep(loci[i], i-1)
     
     }) %>% unlist()
     
-    pop_df0 <- separate(data = tibble(dat = dis[popstart:popend]), col = dat, sep ="[[:blank:]]+", into = c("Pop","Locus1","Locus2","PValue",NA,NA), remove = TRUE) %>% 
-      mutate(`Locus1`= rep(loc1, npops), `Locus2`= rep(loc2, npops), PValue = gsub(PValue, pattern = "No", replacement = "1")) %>% 
-      mutate(PValue = gsub(PValue, pattern = "$$0", replacement = repzero) %>% as.numeric())
+    pop_df0 <- tidyr::separate(data = tibble::tibble(dat = dis[popstart:popend]), col = dat, sep = "[[:blank:]]+", into = c("Pop", "Locus1", "Locus2", "PValue", NA, NA), remove = TRUE) %>% 
+      dplyr::mutate(`Locus1`= rep(loc1, npops), `Locus2`= rep(loc2, npops), PValue = gsub(PValue, pattern = "No", replacement = "1")) %>% 
+      dplyr::mutate(PValue = gsub(PValue, pattern = "$$0", replacement = repzero) %>% as.numeric())
     
   } else {
     
-    pop_df0 <- separate(data = tibble(dat = dis[popstart:popend]), col = dat, sep ="[[:blank:]]+", into = c("Pop","Locus1","Locus2","PValue",NA,NA), remove = TRUE) %>% 
-      mutate(PValue = gsub(PValue, pattern = "No", replacement = "1")) %>% 
-      mutate(PValue = gsub(PValue, pattern = "$$0", replacement = repzero) %>% as.numeric())
+    pop_df0 <- tidyr::separate(data = tibble::tibble(dat = dis[popstart:popend]), col = dat, sep = "[[:blank:]]+", into = c("Pop", "Locus1", "Locus2", "PValue", NA, NA), remove = TRUE) %>% 
+      dplyr::mutate(PValue = gsub(PValue, pattern = "No", replacement = "1")) %>% 
+      dplyr::mutate(PValue = gsub(PValue, pattern = "$$0", replacement = repzero) %>% as.numeric())
     
   } #end Loci
   
@@ -94,25 +94,26 @@ ReadGenepopDis.GCL <- function(file, loci = NULL, sillyvec = NULL) {
     
     pop_names <- lapply(sillyvec, function(silly){
       
-      rep(silly,ncomps)
+      rep(silly, ncomps)
       
     }) %>% 
       unlist()
+    
   }else{
     
     pop_names <- pop_df0 %>% 
-      pull(Pop)
+      dplyr::pull(Pop)
     
   } #end sillyvec
 
   
   pop_df <- pop_df0 %>%
-    mutate(Pop=pop_names) %>% 
-    group_by_at(vars(-PValue)) %>%
-    mutate(row_id = 1:n()) %>% 
-    ungroup() %>%
-    spread(key = Pop, value = PValue) %>% 
-    select(-row_id)
+    dplyr::mutate(Pop = pop_names) %>% 
+    dplyr::group_by(dplyr::across(-PValue)) %>%
+    dplyr::mutate(row_id = 1:n()) %>% 
+    dplyr::ungroup() %>%
+    tidyr::pivot_wider(names_from = Pop, values_from = PValue) %>% 
+    dplyr::select(-row_id)
   
   #npops
   if(npops==1) {
@@ -123,16 +124,16 @@ ReadGenepopDis.GCL <- function(file, loci = NULL, sillyvec = NULL) {
     
     pop_df <- pop_df
     
-    locstart<-popend+6
+    locstart <- popend + 6
     
-    locend<-locstart+ncomps-1
+    locend <- locstart + ncomps - 1
     
-    loc_df <- tibble(dat = dis[locstart:locend]) %>%
-      mutate(dat = substr(dat, start = 46, stop = 54)) %>% 
-      mutate(dat = gsub(pattern = "Highly si", x = dat, replacement = "0.000000") %>% as.numeric()) 
+    loc_df <- tibble::tibble(dat = dis[locstart:locend]) %>%
+      dplyr::mutate(dat = substr(dat, start = 46, stop = 54)) %>% 
+      dplyr::mutate(dat = gsub(pattern = "Highly si", x = dat, replacement = "0.000000") %>% as.numeric()) 
     
     summary_df = pop_df %>% 
-      mutate(Overall = loc_df %>% pull(dat))
+      dplyr::mutate(Overall = loc_df %>% dplyr::pull(dat))
     
   } #end npops
     
