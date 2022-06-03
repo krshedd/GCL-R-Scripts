4bbd5f4a899b056e5a2cab601d0a447cb9c1580b
awbarclay <andy.barclay@alaska.gov>
2022-06-03 13:58:57 -0800

    Added the Loki test database URL as an option.

--- LOKI_URL.GCL.r
+++ LOKI_URL.GCL.r
@@ -1,4 +1,4 @@
-LOKI_URL.GCL <- function(){
+LOKI_URL.GCL <- function(test.db = FALSE){
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #  This function builds the URL to connect to LOKI and is called on by other GCL 
@@ -10,13 +10,27 @@ LOKI_URL.GCL <- function(){
   #  Written by Andy Barclay 4/15/19
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
-  host <- "soaora7-scan.us1.ocm.s7134325.oraclecloudatcustomer.com"
-  
-  port <- "1521"
-  
-  svc <- "dfgcfresp.us1.ocm.s7134325.oraclecloudatcustomer.com"
-  
-  protocol <-"jdbc:oracle:thin:"
+  if(test.db == TRUE){
+    
+    host <- "soaora6-scan.us1.ocm.s7134325.oraclecloudatcustomer.com"
+    
+    port <- "1521"
+    
+    svc <- "DFGGCLT.us1.ocm.s7134325.oraclecloudatcustomer.com"
+    
+    protocol <-"jdbc:oracle:thin:"
+    
+  }else{
+    
+    host <- "soaora7-scan.us1.ocm.s7134325.oraclecloudatcustomer.com"
+    
+    port <- "1521"
+    
+    svc <- "dfgcfresp.us1.ocm.s7134325.oraclecloudatcustomer.com"
+    
+    protocol <-"jdbc:oracle:thin:"
+    
+  }
   
   url <- paste0(protocol, "@", host, ":", port, "/", svc)
   
